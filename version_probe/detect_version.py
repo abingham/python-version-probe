ce44f93050133c3373e5ed5ec1ec8b28e599c343
Austin Bingham <austin.bingham@gmail.com>
2014-09-21 15:31:19 +0100

    Work on basic detection.

--- version_probe/detect_version.py
+++ version_probe/detect_version.py
@@ -1,5 +1,40 @@
+import os
+import os.path
+import subprocess
+
+
+def detect_file_version(filename):
+    """Attempt to detect the major version of the Python code in
+    `filename`.
+
+    Returns: `2` or `3`, depending on the detected version.
+
+    Raises:
+        ValueError: If a parsing error is detected.
+    """
+    with open(os.devnull, 'w') as devnull:
+        try:
+            output = subprocess.check_output(['2to3', filename],
+                                             stderr=devnull)
+        except subprocess.CalledProcessError:
+            raise ValueError('Error parsing {}'.format(filename))
+
+    changes = [o for o in output.split(b'\n') if len(o)]
+    return 2 if len(changes) else 3
+
+
 def detect_version(directory):
     """Attempt to detect the Python major version of the source code in
     `directory`.
     """
-    pass
+
+    version_map = {filename: detect_file_version(filename)
+                   for root, dirs, files in os.walk(directory)
+                   for filename in (os.path.join(root, f) for f in files)}
+    versions = set(version_map.values())
+    if not versions:
+        return None
+    elif len(versions) == 2:
+        return None
+    else:
+        return next(iter(versions))
--- version_probe/test/test_detect_version.py
+++ version_probe/test/test_detect_version.py
@@ -1,10 +1,49 @@
+import os
 import tempfile
 import unittest
 
 import version_probe
+
+
+class Test_detect_file_version(unittest.TestCase):
+    def test_detects_python_v2(self):
+        with tempfile.NamedTemporaryFile(mode='w') as f:
+            f.write('print "llama"')
+            f.flush()
+            self.assertEqual(2, version_probe.detect_file_version(f.name))
+
+    def test_detects_python_v3(self):
+        with tempfile.NamedTemporaryFile(mode='w') as f:
+            f.write('print("foobar")')
+            f.flush()
+            self.assertEqual(3, version_probe.detect_file_version(f.name))
+
+    def test_file_has_syntax_error(self):
+        with tempfile.NamedTemporaryFile(mode='w') as f:
+            f.write('x = "asdf')
+            f.flush()
+            with self.assertRaises(ValueError):
+                version_probe.detect_file_version(f.name)
+
 
 class TestDetectVersion(unittest.TestCase):
     def test_no_files(self):
         with tempfile.TemporaryDirectory() as temp_dir:
             version = version_probe.detect_version(temp_dir)
             self.assertEqual(version, None)
+
+    def test_single_python_v2_file(self):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            filename = os.path.join(temp_dir, 'python_v2.py')
+            with open(filename, 'w') as f:
+                f.write('print "llamas"')
+            version = version_probe.detect_version(temp_dir)
+            self.assertEqual(version, 2)
+
+    def test_single_python_v3_file(self):
+        with tempfile.TemporaryDirectory() as temp_dir:
+            filename = os.path.join(temp_dir, 'python_v3.py')
+            with open(filename, 'w') as f:
+                f.write('print("llamas")')
+            version = version_probe.detect_version(temp_dir)
+            self.assertEqual(version, 3)
