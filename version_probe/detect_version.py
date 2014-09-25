ba95b222099feccc34c1124d2c68513563791378
Austin Bingham <austin.bingham@gmail.com>
2014-09-25 15:10:05 +0200

    Fixed problem where tuple-printing cause incorrect python2 detection.

--- version_probe/detect_version.py
+++ version_probe/detect_version.py
@@ -1,9 +1,53 @@
 # The initial version of this code was written by cjwelborn in
 # http://www.reddit.com/r/Python/comments/2h5pwa/version_probe_module_for_detecting_the_python/
 
+import difflib
 from lib2to3 import refactor
 from lib2to3.pgen2.parse import ParseError
+import re
 import sys
+
+
+def find_matching_paren(s, start_idx):
+    """Find the index of the close-paren matching the open-paren at `idx` in `s`.
+
+    If `s[idx]` is not an open paren, throws `ValueError`. If there is
+    no matching paren, throws `ValueError`.
+    """
+    if s[start_idx] != '(':
+        raise ValueError('No open-paren at specified index')
+
+    balance = 1
+    for idx in range(start_idx + 1, len(s)):
+        if s[idx] == '(':
+            balance += 1
+        elif s[idx] == ')':
+            balance -= 1
+
+        if balance == 0:
+            return idx
+
+    raise ValueError('Unbalance parentheses')
+
+def find_all(s, substr):
+    """Find the starting indices of all instances of `substr` in the
+    string `s`.
+    """
+    return [m.start() for m in re.finditer(substr, s)]
+
+
+def find_print_paren_indices(s):
+    for index in find_all(s, 'print\('):
+        open_paren_index = index + len('print')
+        try:
+            close_paren_index = find_matching_paren(s, open_paren_index)
+            yield (open_paren_index, close_paren_index)
+        except ValueError:
+            pass
+
+
+def insert_substr(s, substr, index):
+    return s[:index] + substr + s[index:]
 
 
 def filter_tuple_printing(old_text, new_text, filename, equal):
@@ -12,12 +56,17 @@ def filter_tuple_printing(old_text, new_text, filename, equal):
     turn `print(1, 2)` into `print((1,2))`. While this is technically
     fine from 2to3's point of view, it doesn't necessarily represent a
     version upgrade. This filter tries to deal with those.
-
     """
-    if old_text.startswith('print(') and len(old_text) + 2 == len(new_text):
-        return False
-    else:
-        return True
+
+    for open_index, close_index in find_print_paren_indices(old_text):
+        assert open_index < close_index, \
+               'Open parens should always come before close parens.'
+        subst = insert_substr(old_text, ')', close_index)
+        subst = insert_substr(subst, '(', open_index)
+        if subst == new_text:
+            return False
+    return True
+
 
 class VersionDetector(refactor.RefactoringTool):
     """A lib2to3 refactoring tool designed to detect if the code being
